0e586bcfc883d14b286730d9758ce1eb8ad253be
Vernell Hill <vernvedomusic@gmail.com>
2017-01-31 15:36:58 -0500

    working modals

--- server/app.js
+++ server/app.js
@@ -1,9 +1,11 @@
+require('dotenv').config();
+
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var sequelize = require('./db.js')
 // build a user model in sqllize
-var User = sequelize.import(__dirname + '/models/user');
+var User = sequelize.import('./models/user');
 
 //creates the table in postgres
 //matches the model we defined
@@ -11,9 +13,11 @@ var User = sequelize.import(__dirname + '/models/user');
 User.sync(); 
 //Danger: THIS WILL DROP THE USER TABLE****
 // User.sync({ force: true }); //drops the table compeletly (line 27ish)
-
+sequelize.sync();
 app.use(bodyParser.json());
 app.use(require('./middleware/headers'));
+app.use(require('./middleware/validate-session'));
+
 app.use('/api/user', require('./routes/user'));
 //login route
 app.use('/api/login', require('./routes/session'));
