188fc0321f676cd56e91c71e600ce2df92be13cc
Vernell Hill <vernvedomusic@gmail.com>
2017-01-31 11:48:06 -0500

    security

--- server/app.js
+++ server/app.js
@@ -15,6 +15,8 @@ User.sync();
 app.use(bodyParser.json());
 app.use(require('./middleware/headers'));
 app.use('/api/user', require('./routes/user'));
+//login route
+app.use('/api/login', require('./routes/session'));
 app.use('/api/test', function(req, res){
 	res.send("Hello World");
 });
