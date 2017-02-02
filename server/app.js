6833177aa3ffe3f9336cbe80509fcd9bdde236c3
Vernell Hill <vernvedomusic@gmail.com>
2017-02-02 09:40:36 -0500

    add new dfinitions

--- server/app.js
+++ server/app.js
@@ -21,10 +21,14 @@ app.use(require('./middleware/validate-session'));
 app.use('/api/user', require('./routes/user'));
 //login route
 app.use('/api/login', require('./routes/session'));
+app.use('/api/definition', require('./routes/definition'));
+
+
+
 app.use('/api/test', function(req, res){
 	res.send("Hello World");
 });
 
 app.listen(3000, function(){
-	console.log("app is listening on port 3000");
+console.log("app is listening on port 3000");
 });
